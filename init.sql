ef1a8c9c4f2062b35ec2a5cb491a8dbea8915c1b
kifuan <kifuan@foxmail.com>
2022-11-04 21:46:53 +0800

    chore: sql script

---
+++ init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `messages` (
+  `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `date` BIGINT NOT NULL,
+	`name` VARCHAR(64) NOT NULL,
+	`content` VARCHAR(256) NOT NULL,
+	`site` VARCHAR(64) NOT NULL,
+	`reply` INT NOT NULL,
+	`email` VARCHAR(64) NOT NULL,
+	`mail_notice` TINYINT(1) NOT NULL
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
